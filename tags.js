62b3a474a1cecfedb90ac2db146c6249e16491ee
Andrii Lavrenchuk <63540148+andrii-lavrenchuk@users.noreply.github.com>
2020-09-29 21:32:55 +0300

    Adds tags

--- tags.js
+++ tags.js
@@ -1,5 +1,29 @@
+// const tagsContainer = document.querySelector('.js-tags');
+// let selectedTag = null;
+
+// tagsContainer.addEventListener('click', onTagsContainerClick);
+
+// function onTagsContainerClick(evt) {
+//   if (evt.target.nodeName !== 'BUTTON') {
+//     return;
+//   }
+
+//   const currentActiveBtn = document.querySelector('.tags__btn--active');
+
+//   if (currentActiveBtn) {
+//     currentActiveBtn.classList.remove('tags__btn--active');
+//   }
+
+//   //   currentActiveBtn?.classList.remove('tags__btn--active');
+
+//   const nextActiveBtn = evt.target;
+//   nextActiveBtn.classList.add('tags__btn--active');
+//   selectedTag = nextActiveBtn.dataset.value;
+//   console.log(selectedTag);
+// }
+
 const tagsContainer = document.querySelector('.js-tags');
-let selectedTag = null;
+const selectedTags = new Set();
 
 tagsContainer.addEventListener('click', onTagsContainerClick);
 
@@ -8,16 +32,17 @@ function onTagsContainerClick(evt) {
     return;
   }
 
-  const currentActiveBtn = document.querySelector('.tags__btn--active');
+  const btn = evt.target;
+  const isBtnActive = btn.classList.contains('tags__btn--active');
+  const tag = btn.dataset.value;
 
-  if (currentActiveBtn) {
-    currentActiveBtn.classList.remove('tags__btn--active');
+  if (isBtnActive) {
+    selectedTags.delete(tag);
+  } else {
+    selectedTags.add(btn.dataset.value);
   }
 
-  //   currentActiveBtn?.classList.remove('tags__btn--active');
+  btn.classList.toggle('tags__btn--active');
 
-  const nextActiveBtn = evt.target;
-  nextActiveBtn.classList.add('tags__btn--active');
-  selectedTag = nextActiveBtn.dataset.value;
-  console.log(selectedTag);
+  console.log(selectedTags);
 }
